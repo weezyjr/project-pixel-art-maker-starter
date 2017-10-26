055182e52a9a6382409f6a9e490640b3b2d4d7b9
Ahmed <w33zy.7@gmail.com>
2017-10-26 17:42:25 +0200

    Use event map

--- designs.js
+++ designs.js
@@ -1,62 +1,41 @@
-$(() => {
-	function makeGrid(w, h) {
-		const tr = '<tr></tr>';
-		const td = '<td></td>';
-		const table = $('#pixel_canvas');
-		let row, pixel;
+function makeGrid(w, h) {
+	const table = $('#pixel_canvas')[0];
 
-		for (let i = 0; i < h; i++) {
-			table.append(tr);
-			row = table.children().last();
-			for (let j = 0; j < w; j++) {
-				row.append(td);
-				pixel = row.children().last();
-				pixel.attr('class', 'pixel');
-			}
+	for (let i = 0; i < h; i++) {
+		const row = table.insertRow(i);
+		for (let j = 0; j < w; j++) {
+			const pixel = row.insertCell(j);
+			$(pixel).on({
+				click: () => {
+					const color = $('#colorPicker').val();
+					//a flag to check if the pixel is already drawed
+					$(pixel).attr('drawed', 'true');
+					$(pixel).css('background-color', color);
+					$(pixel).css('opacity', '1');
+
+				},
+				mouseenter: () => {
+					const color = $('#colorPicker').val();
+					if ($(pixel).attr('drawed') != 'true') {
+						$(pixel).css('background-color', color);
+						$(pixel).css('opacity', '0.7');
+					}
+				},
+				mouseleave: () => {
+					if ($(pixel).attr('drawed') != 'true') {
+						$(pixel).css('background-color', 'initial');
+						$(pixel).css('opacity', '1');
+					}
+				}
+			});
 		}
 	}
+}
 
-	$('#pixel_canvas').on('click', '.pixel', (e) => {
-		let color = $('#colorPicker').val();
-		let pixels = $('.pixel');
-
-		for (const pixel of pixels)
-			if (e.target == pixel) {
-				//a flag to check if the pixel is already drawed
-				$(pixel).attr('drawed', 'true');
-				$(pixel).css('background-color', color);
-				$(pixel).css('opacity', '1');
-			}
-	});
-
-	$('#pixel_canvas').on('mouseenter', '.pixel', (e) => {
-		let color = $('#colorPicker').val();
-		let pixels = $('.pixel');
-
-		for (const pixel of pixels)
-			if (e.target == pixel && $(pixel).attr('drawed') != 'true') {
-				$(pixel).css('background-color', color);
-				$(pixel).css('opacity', '0.7');
-			}
-
-	});
-
-	$('#pixel_canvas').on('mouseleave', '.pixel', (e) => {
-		let pixels = $('.pixel');
-
-		for (const pixel of pixels)
-			if (e.target == pixel && $(pixel).attr('drawed') != 'true') {
-				$(pixel).css('background-color', 'initial');
-				$(pixel).css('opacity', '1');
-			}
-
-	});
-
-	$('#sizePicker').submit((e) => {
-		//Prevent reloading on submit
-		e.preventDefault();
-		const w = $('#input_width').val();
-		const h = $('#input_height').val();
-		makeGrid(w, h);
-	});
+$('#sizePicker').submit((e) => {
+	//Prevent reloading on submit
+	e.preventDefault();
+	const width = $('#input_width').val();
+	const height = $('#input_height').val();
+	makeGrid(width, height);
 });
