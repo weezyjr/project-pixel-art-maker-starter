9f463c3d8a0bfc438d0df723b7d4f95a5ca605d0
Ahmed <w33zy.7@gmail.com>
2017-10-26 16:03:26 +0200

    feat: Add makeGrid function
    Add makeGrid function that constructs the table grid using the width and height parameters

--- designs.js
+++ designs.js
@@ -1,11 +1,26 @@
 $(() => {
-	function makeGrid() {
-		
+	function makeGrid(w, h) {
+		const tr = '<tr></tr>';
+		const td = '<td></td>';
+		const table = $('#pixel_canvas');
+		let row, pixel;
+
+		for (let i = 0; i < h; i++) {
+			table.append(tr);
+			row = table.children().last();
+			for (let j = 0; j < w; j++) {
+				row.append(td);
+				pixel = row.children().last();
+				pixel.attr('class', 'pixel');
+			}
+		}
 	}
-	
-	$('#sizePicker').submit((e)=>{
+
+	$('#sizePicker').submit((e) => {
 		//Prevent reloading on submit
 		e.preventDefault();
-		makeGrid();
+		const w = $('#input_width').val();
+		const h = $('#input_height').val();
+		makeGrid(w, h);
 	});
 });
